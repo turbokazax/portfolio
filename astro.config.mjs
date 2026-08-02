c582bdf90b841a9510d04a0c6c4c38fe61367d1d
turbokazax <endermen2010pro@gmail.com>
2026-08-02 20:25:49 +0500

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [mdx()],
+  site: "https://jubaportfolio.vercel.app/",
+  integrations: [mdx(), sitemap()],
 });
